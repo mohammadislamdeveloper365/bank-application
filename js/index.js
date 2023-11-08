58171a34911b78f7e62475b4e642f63328880b33
Mohammad Moinul Islam <95491483+mohammadislamdeveloper365@users.noreply.github.com>
2023-11-08 23:03:27 +0400

    fix: move common functionalities to a place and code refactor

--- js/index.js
+++ js/index.js
@@ -1,25 +1,35 @@
 let loginButton = getElement('bank-login');
 
 //Add event listener for button
-loginButton.addEventListener('click',function() {
-   let passwordField = getInputFieldValue('password');
-   let usernameField = getInputFieldValue('username');
-   
-  (passwordField === 'secret' && usernameField === 'koti@gmail.com') ? window.location.href = 'home.html': document.getElementById('login-error').innerText = 'Please enter valid email or password';
-});
+loginButton.addEventListener('click',loginFormHandler);
 
 document.addEventListener('keydown',function(event) {
   if(event.key === 'Enter') {
-    let passwordField = getInputFieldValue('password');
-    let usernameField = getInputFieldValue('username');
-    if(passwordField === 'secret' && usernameField === 'koti@gmail.com') {
-      window.location.href = 'home.html';
-    }
-
-    else {
-      document.getElementById('login-error').innerText = 'Please enter valid email or password'
-    }
-  }
-  
+    loginFormHandler();
+}
 });
 
+
+function loginFormHandler() {
+  let passwordField = getFieldValue('password', true);
+   let usernameField = getFieldValue('username', true);
+   let errorField = getElement('error-message');
+   
+   if(errorField) {
+     errorField.parentNode.removeChild(errorField);
+   }
+   if(passwordField === 'secret' && usernameField === 'koti@gmail.com') {
+    window.location.href = 'home.html'
+   } else {
+      const errorField = createElement('p', "Please enter valid email or password");
+      const btn = getElement('bank-login');
+      const form = getElement('bank-login-form');
+      errorField.style.fontSize = '1.5rem';
+      errorField.style.color = '#E21717';
+      errorField.style.textAlign = 'left';
+      errorField.style.paddingBottom = '8px';
+      errorField.id = 'error-message';
+
+      form.insertBefore(errorField, btn );
+   }
+}
--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,24 @@
+function getFieldValue(id, isValue = false) {
+    const element = getElement(id);
+    let value;
+    
+    if(isValue) {
+        value = element.value;   
+    } else {
+        value = element.innerText;
+    }
+
+    return value;
+}
+
+function createElement(elmentType, textNode) {
+    const element = document.createElement(elmentType);
+    const txt = document.createTextNode(textNode);
+    element.appendChild(txt);
+
+    return element;
+}
+
 function getInputFieldValue(id) {
     return getElement(id).value;
 }
