e804b6e1abd085604c6cf316ccb0aedc5baba24f
Mohammad Moinul Islam <95491483+mohammadislamdeveloper365@users.noreply.github.com>
2023-11-09 18:04:49 +0400

    feat: add input validation

--- js/home.js
+++ js/home.js
@@ -22,14 +22,18 @@ getElementById('input-withdraw').addEventListener('keydown',function(event) {
 
 function changeBalanceAmount(balanceFieldId,targetFieldId,inputFieldId,isIncrease) {
     let inputAmount = getNumberOfFieldValue(inputFieldId, true);
-    let depositError = getElementById('deposit-error');
+    let inputValidationField = getElementById('input-field-error');
+    const inputField = getElementById(inputFieldId);
+    
+    if(inputValidationField) {
+        inputValidationField.parentNode.removeChild(inputValidationField);
+    }
 
     if(!isNaN(inputAmount) && inputAmount >= 0) {
         let balanceAmount = getNumberOfFieldValue(balanceFieldId);
         let targetFieldAmount = getNumberOfFieldValue(targetFieldId);
         let targetField = getElementById(targetFieldId);
         let balanceField = getElementById(balanceFieldId);
-        let inputField = getElementById(inputFieldId);
     
         targetFieldAmount = inputAmount + targetFieldAmount;
         targetField.innerText = targetFieldAmount;
@@ -39,9 +43,25 @@ function changeBalanceAmount(balanceFieldId,targetFieldId,inputFieldId,isIncreas
             balanceField.innerText = balanceAmount - inputAmount; 
         }
 
-        inputField.value = '';
-        depositError.innerText = '';
     }
 
-    else depositError.innerText = "Please enter a valid number"
+    else {
+        let targetedElement;
+        let errorField;
+        errorField = createElement('p','Please enter a valid number');
+        errorField.id = 'input-field-error';
+        errorField.style.fontWeight = 'bold';
+        errorField.style.lineHeight = '1.5';
+        errorField.style.color = 'red';
+        
+        if(isIncrease) {
+           targetedElement = getElementById('deposit-btn');
+        } else {
+            targetedElement = getElementById('withdraw-btn');
+        }
+
+        targetedElement.parentNode.insertBefore(errorField, targetedElement);
+    }
+
+    inputField.value = '';
 }
